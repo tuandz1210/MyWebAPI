3cd1ada98dbd247fa3f247bb57b141460a0bd6b1
tuand <tuand@DESKTOP-2TEEH0A>
2022-11-02 21:59:40 +0700

    Add project files.

---
+++ MyWebAPI/Data/BookStoreContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyWebAPI.Data
+{
+    public class BookStoreContext : DbContext
+    {
+        public BookStoreContext(DbContextOptions<BookStoreContext> opt )
+            : base( opt ) 
+        { 
+             
+        }
+
+        #region DBSet
+        public DbSet<Book>? Books { get; set; }
+
+        #endregion
+    }
+}
